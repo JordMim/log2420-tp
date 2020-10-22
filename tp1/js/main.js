19d58737385a0bb9da4376548668d3b96804d7c2
William <ringuet58@gmail.com>
2020-10-22 12:34:22 -0400

    arrangement du pop-up

--- tp1/js/main.js
+++ tp1/js/main.js
@@ -10,36 +10,29 @@ function showmenu(id){
 }
 function hide(id){
   document.getElementById(id).style.display="none";
-  window.history.go(-10);
+  window.location.href="index.html"
+}
+function show(id){
+  document.getElementById(id).style.display="block";
 }
 
 
 function check_pass() {
   var pass = document.formulaire.password.value;
   var confPass = document.formulaire.confPassword.value
-  if (pass === confPass && pass!=="" && confPass!=="") {
+  var passvalidation=/^(?=.*\d)(?=.*[a-zA-Z])/;
+
+
+  if (pass === confPass && pass!=="" && confPass!=="" && pass.match(passvalidation)) {
     document.getElementById('send').disabled = false;
     document.formulaire.password.style.borderBottom="3px solid green";
     document.formulaire.confPassword.style.borderBottom="3px solid green";
-  } else {
+  }
+  else {
+    alert("Le mot de pass ne correspond pas ou pas de mélange de lettres et chiffres!");
     document.formulaire.password.style.borderBottom="3px solid red";
     document.formulaire.confPassword.style.borderBottom="3px solid red";
   }
 }
-function validate(){
-  var pass = document.formulaire.password.value;
-  var passvalidation=/^(?=.*\d)(?=.*[a-zA-Z])/;
-  if(pass.match(passvalidation)){
-    document.getElementById('page_pop').style.display="block";
-    document.getElementById('menu_mission').autofocus=false;
 
-  }
-  else{
-    alert("Mélange de lettres et chiffres!");
-    document.formulaire.password.style.borderBottom="3px solid red";
-    document.formulaire.confPassword.style.borderBottom="3px solid red";
-    return false;
-
-  }
-}
 
