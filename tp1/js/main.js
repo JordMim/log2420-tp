54c3ad7e6c316e255f6301f0a7134d7e501c2c9d
William <ringuet58@gmail.com>
2020-10-22 23:02:25 -0400

    projet final

--- tp1/js/main.js
+++ tp1/js/main.js
@@ -20,20 +20,21 @@ function show(id){
 
 function check_pass() {
   var pass = document.formulaire.password.value;
-  var confPass = document.formulaire.confPassword.value
+  var confPass = document.formulaire.confPassword.value;
   var passvalidation=/^(?=.*\d)(?=.*[a-zA-Z])/;
 
   if (pass === confPass && pass!=="" && confPass!=="" && pass.match(passvalidation)) {
     document.getElementById('send').disabled = false;
     document.formulaire.password.style.borderBottom="3px solid green";
     document.formulaire.confPassword.style.borderBottom="3px solid green";
-    sendJSon();
+    sendJSon()
   }
   else {
-    alert("Le mot de pass ne correspond pas ou pas de mélange de lettres et chiffres!");
+    alert("Le mot de pass ne correspond pas ou pas de mélange de lettres et chiffres!\n N'oubliez pas de cocher la case");
     document.formulaire.password.style.borderBottom="3px solid red";
     document.formulaire.confPassword.style.borderBottom="3px solid red";
   }
+
 }
 
 function sendJSon() {
